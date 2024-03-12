434eebd50cab95cc3d1ec51f62c891d38e776faa
FKM11 <fjcastillomartin@gmail.com>
2024-03-12 16:44:48 +0100

    heredoc

--- src/pipex/heredoc.c
+++ src/pipex/heredoc.c
@@ -6,8 +6,17 @@
 /*   By: frcastil <frcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:48:53 by frcastil          #+#    #+#             */
-/*   Updated: 2024/02/27 17:20:16 by frcastil         ###   ########.fr       */
+/*   Updated: 2024/03/12 13:22:44 by frcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+void	ft_heredoc(t_shell *shell)
+{
+	char	*input;
+	char	*line;
+	int		output;
+
+	input = ft_calloc(1, sizeof(char *));
+}
